cee91b9ad64b627c2ad0b732bcc63b29fd6c415e
yayascheer <yasmin.scheer@escola.pr.gov.br>
2025-05-27 15:56:50 -0300

    Add files via upload
    resolução aula 7-alura

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o maior oceano do mundo?',
+    'O Oceano Pacífico'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é júpiter'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem foi o primeiro homem a pisar na Lua?',
+    'Neil Armstrong, em 1969'
+)
