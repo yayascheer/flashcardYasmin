8dca58ac86a7996a58f5ad40fba67697a51315e8
yayascheer <yasmin.scheer@escola.pr.gov.br>
2025-05-27 20:36:21 -0300

    Add files via upload
    projeto após curso do alura, sem possíveis erros do processo

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o maior oceano do mundo?',
+    'O Oceano Pacífico'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é júpiter'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem foi o primeiro homem a pisar na Lua?',
+    'Neil Armstrong, em 1969'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o idioma mais falado do mundo?',
+    'O mandarim (chinês)'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem pintou a famosa obra "Mona Lisa"?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o menor país do mundo em área territorial?',
+    'O Vaticano'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Que nome se dá ao processo de transformação da lagarta em borboleta?',
+    'Metamorfose'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o maior animal terrestre?',
+    'O Elefante africano'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a moeda oficial da União Europeia?',
+    'O Euro'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que continente fica o deserto do Saara?',
+    'Fica no continente africano'
+)
